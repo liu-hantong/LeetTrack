60e4ed6fb991f9da4dbe81321592860fbe3d72ab
Harley <hantongliu@outlook.com>
2022-12-27 23:53:05 -0800

    Time: 5453 ms (39.95%), Space: 76.2 MB (12.14%) - LeetHub

---
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minKnightMoves(self, x: int, y: int) -> int:
+        direcs = [(2, 1), (-2, -1), (-2, 1), (2, -1), (1, 2), (-1, -2), (-1, 2), (1, -2)]
+        queue = deque([(0, 0, 0)])
+        visited = set()
+        visited.add((0, 0))
+        while queue:
+            l = len(queue)
+            for k in range(l):
+                i, j, step = queue.pop()
+                if i == x and j == y:
+                    return step
+                for m, n in direcs:
+                    _i = i + m
+                    _j = j + n
+                    if (_i, _j) not in visited:
+                        visited.add((_i, _j))
+                        queue.appendleft((_i, _j, step + 1))
